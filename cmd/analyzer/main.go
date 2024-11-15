ef180b337a7fbb11226f4b1b9b84b21030eb90ff
LoveSnowEx <lsktw22@gmail.com>
2024-11-15 22:51:10 +0800

    Comment out job analysis and preprocessing functions for future reference

--- cmd/analyzer/main.go
+++ cmd/analyzer/main.go
@@ -1,64 +1,64 @@
 package main
 
-import (
-	"cake-scraper/pkg/deeplx"
-	"cake-scraper/pkg/htmlparser"
-	"cake-scraper/pkg/job"
-	"encoding/json"
-	"fmt"
-	"log"
-	"os"
-	"strings"
-)
+// import (
+// 	"cake-scraper/pkg/deeplx"
+// 	"cake-scraper/pkg/htmlparser"
+// 	"cake-scraper/pkg/job"
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"strings"
+// )
 
-func readJobs() ([]*job.Job, error) {
-	jobs := []*job.Job{}
+// func readJobs() ([]*job.Job, error) {
+// 	jobs := []*job.Job{}
 
-	data, err := os.ReadFile("jobs.json")
-	if err != nil {
-		return nil, err
-	}
+// 	data, err := os.ReadFile("jobs.json")
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	if err := json.Unmarshal(data, &jobs); err != nil {
-		return nil, err
-	}
+// 	if err := json.Unmarshal(data, &jobs); err != nil {
+// 		return nil, err
+// 	}
 
-	return jobs, nil
-}
+// 	return jobs, nil
+// }
 
-func preprocess(jobs []*job.Job) {
-	for _, j := range jobs[5:6] {
-		var err error
-		jd := htmlparser.Parse(j.Contents["Job Description"])
-		reqs := htmlparser.Parse(j.Contents["Requirements"])
-		jd, err = deeplx.Translate(jd, "ZH-TW", "EN")
-		if err != nil {
-			log.Fatalf("Failed to translate: %v", err)
-		}
-		reqs, err = deeplx.Translate(reqs, "ZH-TW", "EN")
-		if err != nil {
-			log.Fatalf("Failed to translate: %v", err)
-		}
-		j.Contents["Job Description"] = jd
-		j.Contents["Requirements"] = reqs
-	}
-}
+// func preprocess(jobs []*job.Job) {
+// 	for _, j := range jobs[5:6] {
+// 		var err error
+// 		jd := htmlparser.Parse(j.Contents["Job Description"])
+// 		reqs := htmlparser.Parse(j.Contents["Requirements"])
+// 		jd, err = deeplx.Translate(jd, "ZH-TW", "EN")
+// 		if err != nil {
+// 			log.Fatalf("Failed to translate: %v", err)
+// 		}
+// 		reqs, err = deeplx.Translate(reqs, "ZH-TW", "EN")
+// 		if err != nil {
+// 			log.Fatalf("Failed to translate: %v", err)
+// 		}
+// 		j.Contents["Job Description"] = jd
+// 		j.Contents["Requirements"] = reqs
+// 	}
+// }
 
-func main() {
-	jobs, _ := readJobs()
-	preprocess(jobs)
-	analyzer := job.NewJobAnalyzer()
-	summaries := make([]string, len(jobs))
-	for _, j := range jobs {
-		summary, err := analyzer.Analyze(j)
-		if err != nil {
-			log.Fatalf("Failed to analyze job: %v", err)
-		}
-		fmt.Println(summary)
-		summaries = append(summaries, summary)
-	}
-	jsonData := "[\n" + strings.Join(summaries, ",\n") + "\n]"
-	if err := os.WriteFile("summaries.json", []byte(jsonData), 0644); err != nil {
-		log.Fatalf("Failed to write jobs: %v", err)
-	}
-}
+// func main() {
+// 	jobs, _ := readJobs()
+// 	preprocess(jobs)
+// 	analyzer := job.NewJobAnalyzer()
+// 	summaries := make([]string, len(jobs))
+// 	for _, j := range jobs {
+// 		summary, err := analyzer.Analyze(j)
+// 		if err != nil {
+// 			log.Fatalf("Failed to analyze job: %v", err)
+// 		}
+// 		fmt.Println(summary)
+// 		summaries = append(summaries, summary)
+// 	}
+// 	jsonData := "[\n" + strings.Join(summaries, ",\n") + "\n]"
+// 	if err := os.WriteFile("summaries.json", []byte(jsonData), 0644); err != nil {
+// 		log.Fatalf("Failed to write jobs: %v", err)
+// 	}
+// }
--- pkg/job/analyzer.go
+++ pkg/job/analyzer.go
@@ -1,86 +1,86 @@
 package job
 
-import (
-	"context"
-	"encoding/json"
-	"strings"
+// import (
+// 	"context"
+// 	"encoding/json"
+// 	"strings"
 
-	"github.com/tmc/langchaingo/llms"
-	"github.com/tmc/langchaingo/llms/ollama"
-)
+// 	"github.com/tmc/langchaingo/llms"
+// 	"github.com/tmc/langchaingo/llms/ollama"
+// )
 
-const (
-	model        = "llama3.2"
-	systemPrompt = "You are an experienced Software Engineer who is very good at look for jobs."
-)
+// const (
+// 	model        = "llama3.2"
+// 	systemPrompt = "You are an experienced Software Engineer who is very good at look for jobs."
+// )
 
-type JobAnalyzer struct {
-	llm llms.Model
-}
+// type JobAnalyzer struct {
+// 	llm llms.Model
+// }
 
-func NewJobAnalyzer() *JobAnalyzer {
-	llm, err := ollama.New(
-		ollama.WithModel(model),
-		ollama.WithSystemPrompt(systemPrompt),
-	)
-	if err != nil {
-		panic(err)
-	}
-	return &JobAnalyzer{
-		llm: llm,
-	}
-}
+// func NewJobAnalyzer() *JobAnalyzer {
+// 	llm, err := ollama.New(
+// 		ollama.WithModel(model),
+// 		ollama.WithSystemPrompt(systemPrompt),
+// 	)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return &JobAnalyzer{
+// 		llm: llm,
+// 	}
+// }
 
-func extractResponseJson(response string) string {
-	beginPattern := "{\n"
-	endPattern := "\n}"
-	begin := strings.Index(response, beginPattern)
-	end := strings.LastIndex(response, endPattern)
-	if begin == -1 || end == -1 {
-		return response
-	}
-	response = response[begin+len(beginPattern) : end]
-	return strings.Trim(response, " \n")
-}
+// func extractResponseJson(response string) string {
+// 	beginPattern := "{\n"
+// 	endPattern := "\n}"
+// 	begin := strings.Index(response, beginPattern)
+// 	end := strings.LastIndex(response, endPattern)
+// 	if begin == -1 || end == -1 {
+// 		return response
+// 	}
+// 	response = response[begin+len(beginPattern) : end]
+// 	return strings.Trim(response, " \n")
+// }
 
-func (ja *JobAnalyzer) Analyze(j *Job) (string, error) {
-	provideData := j.Contents
-	provideData["Company"] = j.Company
-	provideData["Title"] = j.Title
-	ctx := context.Background()
-	jsonData, err := json.MarshalIndent(provideData, "", "    ")
-	if err != nil {
-		return "", err
-	}
-	resp, err := ja.llm.GenerateContent(
-		ctx,
-		[]llms.MessageContent{
-			{
-				Role: llms.ChatMessageTypeHuman,
-				Parts: []llms.ContentPart{
-					llms.TextContent{
-						Text: `
-Organize the key details from the provided job information in JSON format as follows:
+// func (ja *JobAnalyzer) Analyze(j *Job) (string, error) {
+// 	provideData := j.Contents
+// 	provideData["Company"] = j.Company
+// 	provideData["Title"] = j.Title
+// 	ctx := context.Background()
+// 	jsonData, err := json.MarshalIndent(provideData, "", "    ")
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	resp, err := ja.llm.GenerateContent(
+// 		ctx,
+// 		[]llms.MessageContent{
+// 			{
+// 				Role: llms.ChatMessageTypeHuman,
+// 				Parts: []llms.ContentPart{
+// 					llms.TextContent{
+// 						Text: `
+// Organize the key details from the provided job information in JSON format as follows:
 
-{
-	"Company": "",
-	"Title": "",
-	"Programming Languages": [],
-	"Required Skills": [],
-	"Preferred Skills": []
-}
+// {
+// 	"Company": "",
+// 	"Title": "",
+// 	"Programming Languages": [],
+// 	"Required Skills": [],
+// 	"Preferred Skills": []
+// }
 
-If any information is missing, use "Unknown" as the placeholder. Do not add or modify any fields other than the ones listed above.
-Job information:
-` + "```json\n" + string(jsonData) + "\n```",
-					},
-				},
-			},
-		},
-		llms.WithTemperature(0.5),
-	)
-	if err != nil {
-		return "", err
-	}
-	return extractResponseJson(resp.Choices[0].Content), nil
-}
+// If any information is missing, use "Unknown" as the placeholder. Do not add or modify any fields other than the ones listed above.
+// Job information:
+// ` + "```json\n" + string(jsonData) + "\n```",
+// 					},
+// 				},
+// 			},
+// 		},
+// 		llms.WithTemperature(0.5),
+// 	)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	return extractResponseJson(resp.Choices[0].Content), nil
+// }
