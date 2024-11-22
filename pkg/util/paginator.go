3bb1887f83a3acc660eed5c0f60bc485f3629e3b
LoveSnowEx <lsktw22@gmail.com>
2024-11-22 11:33:26 +0800

    Add Slice and Offset methods to Paginator interface and implement tests

--- pkg/util/paginator.go
+++ pkg/util/paginator.go
@@ -1,6 +1,8 @@
 package util
 
 type Paginator[T any] interface {
+	// Slice returns the slice function
+	Slice(offset, limit int64) []T
 	// CurrentPage returns the current page number
 	CurrentPage() int64
 	// Items returns the items in the current page
@@ -13,6 +15,8 @@ type Paginator[T any] interface {
 	HasNext() bool
 	// Next returns the next page
 	Next() Paginator[T]
+	// Offset returns the offset of the current page
+	Offset() int64
 	// Count returns the number of items in the current page
 	Count() int64
 	// Total returns the total number of items
@@ -32,6 +36,12 @@ type paginator[T any] struct {
 
 func NewPaginator[T any](slice func(offset, limit int64) []T, currentPage, total, perPage int64) Paginator[T] {
 	return &paginator[T]{slice: slice, currentPage: currentPage, total: total, perPage: perPage}
+}
+
+func (p *paginator[T]) Slice(offset, limit int64) []T {
+	offset = min(max(offset, 0), p.total)
+	limit = max(min(limit, p.total-offset), 0)
+	return p.slice(offset, limit)
 }
 
 func (p *paginator[T]) CurrentPage() int64 {
@@ -58,8 +68,12 @@ func (p *paginator[T]) Next() Paginator[T] {
 	return &paginator[T]{slice: p.slice, currentPage: p.currentPage + 1, total: p.total, perPage: p.perPage}
 }
 
+func (p *paginator[T]) Offset() int64 {
+	return (p.currentPage - 1) * p.perPage
+}
+
 func (p *paginator[T]) Count() int64 {
-	return int64(len(p.Items()))
+	return min(p.perPage, p.total-p.Offset())
 }
 
 func (p *paginator[T]) Total() int64 {
---
+++ pkg/util/paginator_test.go
@@ -0,0 +1,75 @@
+package util_test
+
+import (
+	"cake-scraper/pkg/util"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type PaginatorSuite struct {
+	suite.Suite
+	data  []int
+	slice func(offset, limit int64) []int
+}
+
+func (s *PaginatorSuite) TestPaginator() {
+	// Given
+	p := util.NewPaginator(s.slice, 1, 10, 3)
+
+	// When
+	items := p.Items()
+
+	// Then
+	s.Equal([]int{1, 2, 3}, items)
+
+	p2 := util.NewPaginator(s.slice, 2, 10, 3)
+	items2 := p2.Items()
+	s.Equal([]int{4, 5, 6}, items2)
+}
+
+func (s *PaginatorSuite) TestPaginator_HasPrev() {
+	// Given
+	p := util.NewPaginator(s.slice, 1, 10, 3)
+
+	// When
+	hasPrev := p.HasPrev()
+
+	// Then
+	s.False(hasPrev)
+
+	p2 := util.NewPaginator(s.slice, 2, 10, 3)
+	hasPrev2 := p2.HasPrev()
+	s.True(hasPrev2)
+}
+
+func (s *PaginatorSuite) TestPaginator_HasNext() {
+	// Given
+	slice := func(offset, limit int64) []int {
+		return s.data[offset:limit]
+	}
+	p := util.NewPaginator(slice, 1, 10, 3)
+
+	// When
+	hasNext := p.HasNext()
+
+	// Then
+	s.True(hasNext)
+
+	p2 := util.NewPaginator(slice, 4, 10, 3)
+	hasNext2 := p2.HasNext()
+	s.False(hasNext2)
+}
+
+func TestPaginatorSuite(t *testing.T) {
+	data := make([]int, 100)
+	for i := 0; i < 100; i++ {
+		data[i] = i + 1
+	}
+	suite.Run(t, &PaginatorSuite{
+		data: data,
+		slice: func(offset, limit int64) []int {
+			return data[offset : offset+limit]
+		},
+	})
+}
